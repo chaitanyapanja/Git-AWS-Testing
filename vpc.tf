483dca4a9552e7ed9f6409000ed365caefd84c6f
chaitanya <chaitanyapanja987@gmail.com>
2024-08-01 21:29:47 -0500

    second commit

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,18 @@
 resource "aws_vpc" "VPC-1" {
   cidr_block       = "10.10.0.0/16"
   tags = {
-    Name = "main"
+    Name = "VPC-1"
+  }
+}
+resource "aws_vpc" "VPC-2" {
+  cidr_block       = "10.20.0.0/16"
+  tags = {
+    Name = "VPC-2"
+  }
+}
+resource "aws_vpc" "VPC-3" {
+  cidr_block       = "10.30.0.0/16"
+  tags = {
+    Name = "VPC-3"
   }
 }
