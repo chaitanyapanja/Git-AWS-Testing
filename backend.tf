ebf66db5dcfcf990f5881349a2dfaf7757d0f611
chaitanya <chaitanyapanja987@gmail.com>
2024-08-05 11:24:08 -0500

    added backend, vpc, dynamodb

--- backend.tf
+++ backend.tf
@@ -3,5 +3,18 @@ terraform {
     bucket = "aws-s3-austin-chaitanya987"
     key    = "aws-s3-austin-chaitanya987"
     region = "us-east-1"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locking" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,15 @@ resource "aws_vpc" "VPC-1" {
     Name = "VPC-1"
   }
 }
+resource "aws_vpc" "VPC-2" {
+  cidr_block       = "10.20.0.0/16"
+  tags = {
+    Name = "VPC-2"
+  }
+}
+resource "aws_vpc" "VPC-3" {
+  cidr_block       = "10.30.0.0/16"
+  tags = {
+    Name = "VPC-3"
+  }
+}
